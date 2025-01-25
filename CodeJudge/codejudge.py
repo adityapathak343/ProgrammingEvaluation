221f9b97127dc7218aae2858642ae821ff8fb31a
adityapathak343 <39326010+adityapathak343@users.noreply.github.com>
2025-01-25 14:07:42 +0530

    Presentation 25-01-2024

--- CodeJudge/codejudge.py
+++ CodeJudge/codejudge.py
@@ -5,6 +5,13 @@ import json
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import JsonOutputParser
 
+def replaceKeys(inputString, keys, values):
+    if len(keys) != len(values):
+        raise ValueError("Length of keys and values should be the same")
+    for i in range(len(keys)):
+        inputString = inputString.replace("{"+keys[i]+"}", values[i])
+    return inputString
+
 class CodeJudge:
     def __init__(self, promptLocation="./prompts.json", model="gpt-4o-mini") -> None:
         self._loadLLM()
@@ -22,9 +29,22 @@ class CodeJudge:
         if "OPENAI_API_KEY" not in os.environ:
             os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter your OpenAI API key: ")
         return None
+
+    def _loadBinaryEvaluationPrompts(self) -> list[str]:
+        return self.prompts["binaryevaluation"]
+    
+    def _getLLMResponseBinary(self, problem: str, code: str) -> dict:
+        prompts = self._loadBinaryEvaluationPrompts()
+        stageOnePrompt = replaceKeys(prompts[0]["prompt"], ["PROBLEM", "CODE"], [problem, code])
+        stageOneResponse = self.llm.invoke(stageOnePrompt).content
+        print(stageOneResponse)
+        stageTwoPrompt = replaceKeys(prompts[1]["prompt"], ["MISTAKE"], [stageOneResponse])
+        stageTwoResponse = self.llm.invoke(stageTwoPrompt).content
+
+        return stageTwoResponse
     
     def _getLLMResponseTaxonomical(self, problem: str, code: str) -> dict:
-        finalPrompt = self.prompts["faultlocalization"][0]["prompt"].replace(r"{PROBLEM}", problem).replace(r"{CODE}", code)
+        finalPrompt = replaceKeys(self.prompts["faultlocalization"][0]["prompt"], ["PROBLEM", "CODE"], [problem, code])
         jsonLLM = self.llm.bind(response_format={"type": "json_object"})
         aiMsg = jsonLLM.invoke(finalPrompt)
         return json.loads(aiMsg.content)
@@ -40,8 +60,12 @@ class CodeJudge:
         score = 1 + (penalty/100)
         return score
     
-    def evaluateTaxonomical(self, problem:str, code:str) -> tuple:
+    def evaluateTaxonomical(self, problem:str, code:str, referenceCode:str = None) -> dict:
         response = self._getLLMResponseTaxonomical(problem, code)
         score = self._assignScoreTaxonomical(response)
         return {"inconsistencies": response["inconsistencies"], "score": score}
+
+    def evaluateBinary(self, problem:str, code:str, referenceCode:str = None) -> str:
+        response = self._getLLMResponseBinary(problem, code)
+        return response
     
